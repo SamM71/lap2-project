5ccfd92809d6599b8764c4beb99f91d182f18d7d
SamM71 <sammocock@gmail.com>
2023-08-21 12:51:43 +0100

    create customers table

--- database/setup.sql
+++ database/setup.sql
@@ -8,9 +8,26 @@ CREATE TABLE accounts (
   PRIMARY KEY (account_id)
 );
 
+CREATE TABLE customers (
+  customer_id INT GENERATED ALWAYS AS IDENTITY,
+  account_id INT,
+  active_requests INT DEFAULT 0,
+  item_sale INT DEFAULT 0,
+  PRIMARY KEY (customer_id),
+  CONSTRAINT fk_account
+    FOREIGN KEY (account_id)
+    REFERENCES accounts(account_id)
+);
+
 INSERT INTO accounts
   (email, user_name, user_password)
 VALUES
   ('sam@example.com', 'Sam Mocock', 'jkljkl'),
   ('ravia@example.com', 'Ravia Saini', 'jkjk'),
   ('nathan@example.com', 'Nathan Smith', 'jkl');
+
+INSERT INTO customers
+  (account_id, active_requests)
+VALUES
+  (1, 2),
+  (2, 1)
